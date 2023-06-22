364edee119146f824749fa170e9c40180cd7c03f
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-06-22 16:15:50 -0400

    UPDATE info

--- functions/src/candy.js
+++ functions/src/candy.js
@@ -21,3 +21,15 @@ export async function addNewCandy(req, res) {
         res.status(500).send(err);
     }
 }
+
+export async function updateCandById(req, res) {
+    try {
+      //const candyId = req.params.candyId
+      const { candyId } = req.params; // which candy are we changing 
+      const updatedInfo = req.body; // what changes are we making (e.g { price: 2.99 })
+      await coll.doc(candyId).update(updatedInfo); // updated the doc with new info    
+      getAllCandy(req, res);
+    } catch(err) {
+        res.status(500).send(err);
+    }
+}
